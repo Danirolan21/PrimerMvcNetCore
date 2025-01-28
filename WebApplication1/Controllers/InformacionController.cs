48cb29b344fc2d505b9e158a264375dba20637ff
Danirolan21 <danirolan21@hotmail.com>
2025-01-28 22:31:20 +0100

    Commit Get & Post

--- WebApplication1/Controllers/InformacionController.cs
+++ WebApplication1/Controllers/InformacionController.cs
@@ -5,6 +5,38 @@ namespace PrimerMvcNetCore.Controllers
 {
     public class InformacionController : Controller
     {
+        public IActionResult VistaControllerPost()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult VistaControllerPost
+            (Persona persona, string aficiones)
+        {
+            ViewData["DATA"] = "Nombre: " + persona.Nombre
+                + ", Email: " + persona.Email
+                + ", Edad: " + persona.Edad
+                + ", Aficiones: " + aficiones;
+            return View();
+        }
+        public IActionResult VistaControllerGet
+            (string saludo, int? year)
+        {
+            //PARA COMPROBAR QUE LOS HEMOS RECIBIDO,
+            //GUARDAMOS LOS PARAMETROS EN ViewData
+            //PARA VER LA INFORMACION
+            //ViewData["DATA"] = "Hola " + saludo
+            //    + " en el año " + year;
+            if (year != null)
+            {
+                ViewData["DATA"] = "Hola " + saludo
+                + " en el año " + year;
+            } else
+            {
+                ViewData["DATA"] = "Aqui no tenemos tiempo";
+            }
+            return View();
+        }
         public IActionResult ControladorVista()
         {
             //VAMOS A ENVIAR INFORMACION SIMPLE A NUESTRA VISTA
